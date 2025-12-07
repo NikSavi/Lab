c2e6a3e1fe38cbe47b700a4683494454c0959fe1
NikSavi <132820183+NikSavi@users.noreply.github.com>
2025-12-07 16:58:52 +0300

    Delete seminar_3 directory

--- seminar_3/task_2/fun.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-int get_array(char array[]);
-void get_alg(char array_1[], char array_2[], int reg_1, int reg_2);
--- seminar_3/task_3/fun.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-void print_horizontal(int array[]);
-int input(char array[]);
-void print_vertical(int array[]);
-int count_num(char array[], int dia[], int point);
